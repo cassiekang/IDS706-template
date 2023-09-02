d2ac2a282e3fac84089a0fe4ca37e8eafb14e96c
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-09-02 13:13:36 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,8 +3,21 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from main import add
 
+class TestAddFunction(unittest.TestCase):
 
-def test_add():
-    assert add(1, 2) == 3
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(3, 5), 8)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-3, -5), -8)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(3, -5), -2)
+
+if __name__ == '__main__':
+    test_add_positive_numbers()
+    test_add_negative_numbers()
+    test_add_mixed_numbers()
+    print("All tests passed.")
