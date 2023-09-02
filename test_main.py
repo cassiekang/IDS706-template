e0fdcf45800ef2430657b7f92ac4c17414bdead0
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-09-02 13:16:16 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,16 +5,14 @@ Test goes here
 
 from main import add
 
-class TestAddFunction(unittest.TestCase):
+def test_add_positive_numbers():
+    assert add(3, 5) == 8
 
-    def test_add_positive_numbers(self):
-        self.assertEqual(add(3, 5), 8)
+def test_add_negative_numbers():
+    assert add(-3, -5) == -8
 
-    def test_add_negative_numbers(self):
-        self.assertEqual(add(-3, -5), -8)
-
-    def test_add_mixed_numbers(self):
-        self.assertEqual(add(3, -5), -2)
+def test_add_mixed_numbers():
+    assert add(3, -5) == -2
 
 if __name__ == '__main__':
     test_add_positive_numbers()
